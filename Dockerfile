f728f6df28c7c7c00ad86fafc227ee52ffa71994
Matt Godbolt <matt@godbolt.org>
2023-03-04 16:50:00 -0600

    Minimal build

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,18 @@
 FROM ubuntu:20.04
-MAINTAINER Matt Godbolt <matt@godbolt.org>
 
 ARG DEBIAN_FRONTEND=noninteractive
-RUN apt update -y -q && apt upgrade -y -q && apt upgrade -y -q && apt install -y -q \
-    bison \
+RUN apt update -y -q && apt upgrade -y -q
+RUN apt install -y -q \
+    autoconf \
     bzip2 \
     curl \
-    file \
-    flex \
-    gawk \
     gcc \
     git \
-    libc6-dev-i386 \
-    libelf-dev \
-    linux-libc-dev \
+    libdb5.3-dev \
+    libgmp-dev \
     make \
-    patch \
-    texinfo \
-    unzip \
-    wget \
     xz-utils
 
-WORKDIR /opt/compiler-explorer/gcc-build
+COPY build /build
+
+WORKDIR /build
--- build/build.sh
+++ build/build.sh
@@ -3,12 +3,13 @@
 set -ex
 
 VERSION=$1
-URL=https://sourceforge.net/projects/gnucobol/files/gnucobol/${VERSION}/gnucobol-${VERSION}-rc2.tar.bz2
+MAJOR_MIN=$(echo ${VERSION} | cut -d- -f1)
+URL=https://sourceforge.net/projects/gnucobol/files/gnucobol/${MAJOR_MIN}/gnucobol-${VERSION}.tar.bz2
 
 FULLNAME=gnucobol-${VERSION}
 OUTPUT=$2/${FULLNAME}.tar.xz
 
-REVISION="cobol-${VERSION}"
+REVISION="gnucobol-${VERSION}"
 LAST_REVISION="${3}"
 
 echo "ce-build-revision:${REVISION}"
@@ -20,15 +21,17 @@ if [[ "${REVISION}" == "${LAST_REVISION}" ]]; then
 fi
 
 STAGING_DIR=$(pwd)/staging
-mkdir -p "${STAGING_DIR}"
+rm -rf ${STAGING_DIR}
 
-cd "${STAGING_DIR}"
-curl -sL ${URL} | tar --strip-components=1 Jxf -
-
+rm -rf build
+mkdir build
+pushd build
+curl -sL ${URL} | tar jxf - --strip-components=1
 ./autogen.sh
 ./configure
 make -j$(nproc)
 make DESTDIR=${STAGING_DIR} prefix=${STAGING_DIR} install
+popd
 
 export XZ_DEFAULTS="-T 0"
 tar Jcf "${OUTPUT}" --transform "s,^./,./cobol-${VERSION}/," -C "${STAGING_DIR}" .
